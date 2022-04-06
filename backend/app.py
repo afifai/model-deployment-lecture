747ecc8b4c0e7643026a5f8bf343997305d757e7
Afif A. Iskandar <surel.afifai@gmail.com>
2022-04-06 16:54:55 +0700

    6 april 2022

--- backend/app.py
+++ backend/app.py
@@ -4,38 +4,35 @@ import pandas as pd
 
 app = Flask(__name__)
 
-with open("final_pipe.pkl", "rb") as model_file:
-    model = pickle.load(model_file)
+with open("pipeline_new.pkl", "rb") as f:
+    model = pickle.load(f)
 
-columns=['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']
+columns = ['Pclass',  'Fare', 'Age', 'SibSp', 'Parch', 'Sex']
 classes = ['Not Survived', 'Survived']
 
+@app.route("/")
+def home():
+    return "<h1>It Works!</h1>"
 
-@app.route("/") # home page
-def hello_world():
-    return "<h1>It Works</h1>"
-
-@app.route("/predict", methods=['GET', 'POST'])
+@app.route("/predict", methods=['GET','POST'])
 def model_prediction():
     if request.method == "POST":
         content = request.json
-        
         try:
-            data = [content['passenger_class'],
-                    content['gender'],
-                    content['age'],
-                    content['siblingspouse'],
-                    content['parentchildren']]
+            data= [content['passenger_class'],
+                   content['fare'],
+                   content['age'],
+                   content['siblingspouse'],
+                   content['parentchildren'],
+                   content['gender']]
             data = pd.DataFrame([data], columns=columns)
             res = model.predict(data)
-            response = {'code':200,
-                        'status':"OK",
-                        'data':{'result':{'target':str(res[0]),
-                                        'target_names':classes[res[0]]}}}
+            response = {"code": 200, "status":"OK", 
+                        "result":{"prediction":str(res[0]),
+                                   "description":classes[res[0]]}}
             return jsonify(response)
         except Exception as e:
-            response = {'code':500,
-                        'status':"error",
-                        'summary': str(e)}
+            response = {"code":500, "status":"ERROR", 
+                        "result":{"error_msg":str(e)}}
             return jsonify(response)
-    return "Silahkan gunakan method post untuk mengakses hasil prediksi dari model"
+    return "<p>Silahkan gunakan method POST untuk mengakses hasil prediksi dari model</p>"
--- frontend/app.py
+++ frontend/app.py
@@ -3,26 +3,27 @@ import requests
 
 
 st.title("Aplikasi Pengecekan Penumpang Titanic")
-pclass = st.number_input("Passenger Class")
+pclass = st.selectbox("Passenger Class", [1, 2, 3])
+fare = st.number_input("Fare")
 age = st.number_input("Age")
 sibsp = st.number_input("Sibling / Spouse")
 parch = st.number_input("Parent / Children")
-# sex = st.selectbox("Gender", ['male', 'female'])
-sex = st.text_input("Gender")
+sex = st.selectbox("Gender", ['male', 'female'])
 # inference
 data = {'passenger_class':pclass,
+        'fare':fare,
         'age': age,
         'siblingspouse':sibsp,
         'parentchildren':parch,
         'gender':sex}
 
-URL = "https://h8-backend-deployment.herokuapp.com/predict"
+URL = "https://model-deployment9-backend.herokuapp.com/predict"
 
 # komunikasi
 r = requests.post(URL, json=data)
 res = r.json()
 if res['code'] == 200:
-    st.title(res['data']['result']['target_names'])
+    st.title(res['result']['description'])
 else:
     st.write("Mohon maaf terjadi kesalahan")
-    st.write(f"keterangan : {res['summary']}")
+    st.write(f"keterangan : {res['result']['error_msg']}")
